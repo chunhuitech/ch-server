94dc36c94857d2526d0847372c2e081fa8169a91
HECHENGJIN <HECHENGJIN@KINGSOFT.COM>
2018-11-05 10:12:30 +0800

    修改swagger地址

--- ch-server-build/ch-apis/ch-api-admin/src/main/java/cn/chunhuitech/www/api/admin/config/SwaggerConfig.java
+++ ch-server-build/ch-apis/ch-api-admin/src/main/java/cn/chunhuitech/www/api/admin/config/SwaggerConfig.java
@@ -31,11 +31,12 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("www.chunhuitech.cn")
                 .apiInfo(apiInfo())
                 .useDefaultResponseMessages(false)
                 .globalOperationParameters(globalOperationParameters())// 全局参数
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("cn.chunhuitech.www.api.admin.controller"))//RequestHandlerSelectors.any()
+                .apis(RequestHandlerSelectors.basePackage("cn.chunhuitech.www.api.admin.controller.api"))//RequestHandlerSelectors.any()
                 .paths(PathSelectors.any())// 监控所有路径 //PathSelectors.regex("/.*")
                 .build();
     }
